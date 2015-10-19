8b3f6a1264a7dabdd0093d57bb2539614dad4604
Miguel Salazar <miguelsalazarg@gmail.com>
2015-10-19 00:15:01 -0500

    Added Bootstrap

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import jsonify, request, render_template, url_for
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
+@app.route("/index.htm")
+@app.route("/index.html")
 def index():
-    return "Hello, World!"
+	return render_template("index.html")
